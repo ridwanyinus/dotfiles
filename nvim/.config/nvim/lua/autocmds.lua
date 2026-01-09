f0bf36c0b1b0f20ecf3727ee8f3e02160bff4c1f
ridwan <ridwanyinusa15@gmail.com>
2026-01-09 20:56:25 +0100

    feat: enhance Neovim configuration with chadwal theme and dashboard improvements
    This commit includes:
    - Added chadwal theme integration with pywal color support
    - Enhanced dashboard with custom ASCII art and improved autocmds
    - Added pywal scripts for dynamic theming (dark/light/matugen)
    - Updated statusline with git integration and minimal theme
    - Improved buffer management for automatic dashboard display

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -1,9 +1,52 @@
 require "nvchad.autocmds"
-
 local api = vim.api
 
 api.nvim_create_autocmd("BufEnter", {
     callback = function()
         vim.cmd "silent! lcd %:p:h"
+    end,
+})
+
+local function has_real_buffers()
+    local buffers = api.nvim_list_bufs()
+    local real_buffers = 0
+
+    for _, buf in ipairs(buffers) do
+        if api.nvim_buf_is_valid(buf) and api.nvim_buf_is_loaded(buf) then
+            local buftype = api.nvim_get_option_value("buftype", { buf = buf })
+            local bufname = api.nvim_buf_get_name(buf)
+            local listed = api.nvim_get_option_value("buflisted", { buf = buf })
+
+            if buftype == "" and listed then
+                if bufname ~= "" or api.nvim_get_option_value("modified", { buf = buf }) then
+                    real_buffers = real_buffers + 1
+                end
+            end
+        end
+    end
+
+    return real_buffers > 0
+end
+
+api.nvim_create_autocmd({ "BufDelete", "BufWipeout" }, {
+    group = api.nvim_create_augroup("ShowDashboard", { clear = true }),
+    callback = function(args)
+        local buftype = api.nvim_get_option_value("buftype", { buf = args.buf })
+        if buftype ~= "" then
+            return
+        end
+
+        vim.schedule(function()
+            vim.defer_fn(function()
+                if not has_real_buffers() then
+                    local current_buf = api.nvim_get_current_buf()
+                    local current_ft = api.nvim_get_option_value("filetype", { buf = current_buf })
+
+                    if current_ft ~= "nvdash" then
+                        vim.cmd "Nvdash"
+                    end
+                end
+            end, 10)
+        end)
     end,
 })
--- nvim/.config/nvim/lua/chadrc.lua
+++ nvim/.config/nvim/lua/chadrc.lua
@@ -2,11 +2,25 @@
 -- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
 -- Please read that file to know all available options :(
 
----@type ChadrcConfig
 local M = {}
+local git_fn = function()
+    if
+        not vim.b[vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)].gitsigns_head
+        or vim.b[vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)].gitsigns_git_status
+    then
+        return ""
+    end
+
+    local git_status = vim.b[vim.api.nvim_win_get_buf(vim.g.statusline_winid or 0)].gitsigns_status_dict
+
+    local branch_name = " " .. git_status.head
+
+    return " " .. branch_name
+end
 
 M.base46 = {
-    theme = "doomchad",
+    theme = "chadwal",
+    theme_toggle = { "chadwal", "doomchad" },
     transparency = true,
     hl_override = {
         Comment = { italic = true },
@@ -14,11 +28,72 @@ M.base46 = {
     },
 }
 
-M.nvdash = { load_on_startup = true }
+M.nvdash = {
+    load_on_startup = true,
+    header = {
+        "⣿⣿⣿⣟⠿⠙⠌⠈⠈⠁⠀⠀⠀⠀⠁⠀⠈⠘⢻⣽⣿⣿⡿⢛⠿⠋⠽⠿⠁⠀",
+        "⣿⣿⣿⡑⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠁⣹⣿⠿⠁⠃⠀⠀⠀⠀⠀⠀",
+        "⣿⡎⠃⠀⠀⠀⠀⠀⠠⡀⠀⠁⠀⠀⠀⠀⠀⠀⠀⠀⣻⡆⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⣿⣓⠀⠀⠀⠀⠀⠤⠈⠀⣨⡴⠆⢀⠀⠀⠀⠀⠂⠀⠹⠁⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⣿⡀⠀⠀⠀⡀⠀⠀⠠⠀⢉⣤⣷⣄⣤⠄⠀⠀⠂⠀⠀⠀⠀⠀⠀⠁⠀⠀⠀⠀",
+        "⣿⣷⡤⠀⠂⣁⢠⡐⠒⢪⣭⡿⠙⠙⠀⠀⠀⠀⠄⠀⠀⠄⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⣿⣿⣷⣆⡨⡎⣩⣠⡠⢋⣕⡮⠆⠃⠀⠈⠀⠈⠌⠀⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⣿⣿⣿⣿⣷⡌⡗⠏⣠⠗⠁⠀⠀⠀⠀⠀⢀⠙⠄⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⣿⣿⣿⣿⣿⣿⣌⠀⠀⠀⠀⠀⠀⢠⡿⠀⡬⠂⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⣿⣿⣿⣿⣿⣿⣿⣿⢦⡀⠀⠀⠐⠋⡁⣜⡞⡠⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⣿⣿⣿⣿⡏⣻⣿⠋⠑⠀⠀⠀⠀⠈⢊⡴⠊⣁⡄⠀⠀⠀⠀⠀⠀⠐⠀⠀⠀⠀",
+        "⣿⣿⣿⡿⠀⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠰⠊⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⣿⣿⣿⠀⠀⠀⠀⠀⠀⠀⠄⠤⠄⠀⠀⠀⠀⠀⠀⠀⠀⠄⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⡿⠩⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        "⡇⠀⠀⠀⠈⠀⠂⠀⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀",
+        "",
+    },
+
+    buttons = {
+        { txt = "  Find File", keys = "ff", cmd = "Telescope find_files" },
+        { txt = "  Recent Files", keys = "fo", cmd = "Telescope oldfiles" },
+
+        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+
+        {
+            txt = function()
+                local stats = require("lazy").stats()
+                local ms = math.floor(stats.startuptime) .. " ms"
+                return "  Loaded " .. stats.loaded .. "/" .. stats.count .. " plugins in " .. ms
+            end,
+            hl = "NvDashFooter",
+            no_gap = true,
+            content = "fit",
+        },
+
+        { txt = "─", hl = "NvDashFooter", no_gap = true, rep = true },
+    },
+}
+
 M.ui = {
     tabufline = {
         lazyload = false,
     },
+
+    statusline = {
+        theme = "minimal",
+        order = { "mode", "cgit", "%=", "lsp_msg", "%=", "lsp", "cwd" },
+        modules = {
+            cgit = function()
+                return "%#St_gitIcons#" .. git_fn()
+            end,
+        },
+    },
+
+    cmp = {
+        format_colors = {
+            tailwind = true,
+        },
+    },
+}
+
+M.cheatsheet = {
+    theme = "simple", -- simple/grid
 }
 
 return M
