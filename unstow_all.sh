86c9192174f52f971d0662f6a71bcb58b372dbe2
ridwan <ridwanyinusa15@gmail.com>
2026-02-25 23:26:01 +0100

    chore: update assets and remove deprecated configs

--- unstow_all.sh
+++ unstow_all.sh
@@ -2,8 +2,7 @@
 
 DOTFILES="$HOME/dotfiles"
 
-# Loop over all directories in your dotfiles folder and unstow them
 for dir in "$DOTFILES"/*/; do
-    # Remove the symlinks created by stow
-    stow -v -D -d "$DOTFILES" "$(basename "$dir")"
+  # Remove the symlinks created by stow
+  stow -v -D -d "$DOTFILES" "$(basename "$dir")"
 done
